664fe982668f481bab2927b6de692f481153530a
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-24 15:19:05 +0530

    created homepage, homepage is accessible from login page
    by clicking the login button on todo login page it redirects to homepage
    in homepage one can enter title and description and submit

--- src/webApp.js
+++ src/webApp.js
@@ -2,25 +2,28 @@ const fs = require("fs");
 const { Framework } = require("./webFramework.js");
 //const app = new Framework
 
-const sendData = function (req, res, data) {
+const sendData = function(req, res, data) {
   res.statusCode = 200;
   res.write(data);
   res.end();
 };
 
-const sendNotFound = function (req, res) {
+const sendNotFound = function(req, res) {
   res.statusCode = 404;
+  res.write("Not Found");
   res.end();
-}
+};
 
-const getFilePath = function (url) {
+const getFilePath = function(url) {
   let filePath = "./public" + url;
-  if (url === "/") { filePath = "./public/index.html"; }
+  if (url === "/") {
+    filePath = "./public/index.html";
+  }
   return filePath;
 };
 
-const serveFiles = function (req, res) {
-  const filePath = getFilePath(req.url)
+const serveFiles = function(req, res) {
+  const filePath = getFilePath(req.url);
   fs.readFile(filePath, (error, data) => {
     if (!error) {
       sendData(req, res, data);
@@ -30,12 +33,10 @@ const serveFiles = function (req, res) {
   });
 };
 
-const app = function (req, res) {
+const app = function(req, res) {
   const framework = new Framework();
   framework.use(serveFiles);
   framework.handleRequest(req, res);
-
-
 };
 
 module.exports = app;

767928c1a11271ff6e53a4ed645d4a58ecba5b62
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-24 18:15:36 +0530

    storing todo in data base and modified homepage.html

---
+++ public/js/homepage.js
@@ -0,0 +1,22 @@
+/* global document, window, fetch, DOMParser */
+/* eslint no-unused-vars: 0 */
+
+const fetchText = request => request.text();
+
+/*
+const convertTextToHtml = function(source) {
+  console.log(source)
+  const parser = new DOMParser();
+  return parser.parseFromString(source, "text/html");
+};
+
+const updateTodoList = function() {
+  fetch("/homepage.html")
+  .then(fetchText)
+  .then((text)=>{
+  });
+};
+
+window.onload = updateTodoList;
+
+*/
--- src/webApp.js
+++ src/webApp.js
@@ -33,8 +33,50 @@ const serveFiles = function(req, res) {
   });
 };
 
+const readBody = function(req, res, next) {
+  let content = "";
+  res.statusCode = 200;
+  req.on("data", chunk => (content += chunk));
+  req.on("end", () => {
+    req.body = content;
+    next();
+  });
+};
+
+const readArgs = text => {
+  let args = {};
+  const splitKeyValue = pair => pair.split("=");
+  const assignKeyValueToArgs = ([key, value]) => (args[key] = value);
+  text
+    .split("&")
+    .map(splitKeyValue)
+    .forEach(assignKeyValueToArgs);
+  return args;
+};
+
+const addNewTodo = function(req, res) {
+  const currentTodo = readArgs(req.body);
+  const todoList = fs.readFileSync("./dataBase/todoList.json", "utf8");
+  const prevTodo = JSON.parse(todoList);
+  prevTodo.push(currentTodo);
+  fs.writeFile("./dataBase/todoList.json", JSON.stringify(prevTodo), err => console.log(err));
+};
+
+
+
+const renderHomepage = function(req, res) {
+  fs.readFile("./public/homepage.html", (err, data) => {
+    const html = data.toString();
+    const message = html.replace("<!--todo_lists-->", "hi this is replaced");
+    addNewTodo(req, res);
+    sendData(req, res, html);
+  });
+};
+
 const app = function(req, res) {
   const framework = new Framework();
+  framework.use(readBody);
+  framework.post("/homepage.html", renderHomepage);
   framework.use(serveFiles);
   framework.handleRequest(req, res);
 };

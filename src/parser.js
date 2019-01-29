eab20c83d667d49733fc47b4a7d83c77e101df26
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-29 22:27:40 +0530

    decoder is used in parser (readArgs) instead of in readBody

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,11 @@
+const { decoder } = require("./utils.js");
+
 const readArgs = function(text) {
   let args = {};
+  const decodedText = decoder(text);
   const splitKeyValue = pair => pair.split("=");
   const assignKeyValueToArgs = ([key, value]) => (args[key] = value);
-  text
+  decodedText
     .split("&")
     .map(splitKeyValue)
     .forEach(assignKeyValueToArgs);
